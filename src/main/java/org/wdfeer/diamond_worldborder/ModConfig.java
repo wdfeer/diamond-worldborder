d12d9054812f137c2ba503d88188112050cdd2c7
wdfeer <49950781+wdfeer@users.noreply.github.com>
2025-12-05 18:54:42 +0100

    Replace midnightlib ModConfig with a record

---
+++ src/main/java/org/wdfeer/diamond_worldborder/ModConfig.java
@@ -0,0 +1,5 @@
+package org.wdfeer.diamond_worldborder;
+
+public record ModConfig(String diamondId, double widthPerDiamond, double timePerDiamondSeconds) {
+    public static final ModConfig DEFAULT = new ModConfig("minecraft:diamond", 1, 10);
+}
