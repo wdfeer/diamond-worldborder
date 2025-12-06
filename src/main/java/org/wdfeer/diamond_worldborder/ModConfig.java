a25e261a510a4f2f9866247c1ddd72fe29add3f3
wdfeer <49950781+wdfeer@users.noreply.github.com>
2025-12-06 06:19:31 +0100

    Implement saveConfig

--- src/main/java/org/wdfeer/diamond_worldborder/ModConfig.java
+++ src/main/java/org/wdfeer/diamond_worldborder/ModConfig.java
@@ -1,12 +1,12 @@
 package org.wdfeer.diamond_worldborder;
 
 import com.google.gson.Gson;
-import com.google.gson.JsonIOException;
 import com.google.gson.JsonSyntaxException;
 import net.fabricmc.loader.api.FabricLoader;
 
-import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.nio.file.Path;
 
 public record ModConfig(String diamondId, double widthPerDiamond, double timePerDiamondSeconds) {
@@ -18,26 +18,35 @@ public record ModConfig(String diamondId, double widthPerDiamond, double timePer
         if (configPath.toFile().exists()) {
             try {
                 return loadConfig();
-            } catch (FileNotFoundException e) {
-                throw new RuntimeException(e);
             } catch (JsonSyntaxException e) {
                 DiamondWorldBorder.LOGGER.error("Invalid config JSON syntax! Using default config...");
                 return DEFAULT;
             }
         } else {
-            saveConfig(DEFAULT);
+            try {
+                saveConfig(DEFAULT);
+            } catch (IOException e) {
+                throw new RuntimeException("Failed saving default config! " + e);
+            }
             return DEFAULT;
         }
     }
 
-    public static ModConfig loadConfig() throws FileNotFoundException, JsonIOException, JsonSyntaxException {
+    public static ModConfig loadConfig() throws JsonSyntaxException {
         var file = configPath.toFile();
-        var reader = new FileReader(file);
-        var gson = new Gson();
-        return gson.fromJson(reader, ModConfig.class);
+        try (var reader = new FileReader(file)) {
+            var gson = new Gson();
+            return gson.fromJson(reader, ModConfig.class);
+        } catch (IOException e) {
+            throw new RuntimeException("Failed reading config! " + e);
+        }
     }
 
-    public static void saveConfig(ModConfig config) {
-        // TODO: implement saving `config` to file
+    public static void saveConfig(ModConfig config) throws IOException {
+        var file = configPath.toFile();
+        try (var writer = new FileWriter(file)) {
+            var gson = new Gson();
+            writer.write(gson.toJson(config));
+        }
     }
 }
