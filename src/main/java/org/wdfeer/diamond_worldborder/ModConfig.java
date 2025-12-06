fc17f672e95e7cc452687449159b423e0f4ab569
wdfeer <49950781+wdfeer@users.noreply.github.com>
2025-12-06 06:13:06 +0100

    Implement loading config

--- src/main/java/org/wdfeer/diamond_worldborder/ModConfig.java
+++ src/main/java/org/wdfeer/diamond_worldborder/ModConfig.java
@@ -1,16 +1,40 @@
 package org.wdfeer.diamond_worldborder;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonIOException;
+import com.google.gson.JsonSyntaxException;
+import net.fabricmc.loader.api.FabricLoader;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.nio.file.Path;
+
 public record ModConfig(String diamondId, double widthPerDiamond, double timePerDiamondSeconds) {
     public static final ModConfig DEFAULT = new ModConfig("minecraft:diamond", 1, 10);
 
+    private static final Path configPath = FabricLoader.getInstance().getConfigDir().resolve("diamond_worldborder.json");
+
     public static ModConfig init() {
-        // TODO: save default config if necessary
-        return loadConfig();
+        if (configPath.toFile().exists()) {
+            try {
+                return loadConfig();
+            } catch (FileNotFoundException e) {
+                throw new RuntimeException(e);
+            } catch (JsonSyntaxException e) {
+                DiamondWorldBorder.LOGGER.error("Invalid config JSON syntax! Using default config...");
+                return DEFAULT;
+            }
+        } else {
+            saveConfig(DEFAULT);
+            return DEFAULT;
+        }
     }
 
-    public static ModConfig loadConfig() {
-        // TODO: implement loading config from file
-        return DEFAULT;
+    public static ModConfig loadConfig() throws FileNotFoundException, JsonIOException, JsonSyntaxException {
+        var file = configPath.toFile();
+        var reader = new FileReader(file);
+        var gson = new Gson();
+        return gson.fromJson(reader, ModConfig.class);
     }
 
     public static void saveConfig(ModConfig config) {
